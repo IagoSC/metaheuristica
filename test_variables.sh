03dbf8c8a3060bce1fb60c82a6de7bde75774a23
iagosc <iagosousavix@gmail.com>
2025-02-11 00:36:00 -0300

    Improve variables ordering

--- test_variables.sh
+++ test_variables.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Problem Set
-problems=(  "A/A-n32-k5.vrp" "A/A-n80-k10.vrp" "A/A-n46-k7.vrp" "B/B-n78-k10.vrp"  "B/B-n50-k8.vrp" )
-pop_size=(30 60) 
-tournament_size=(5) # Grau de competitividade para reprodução
-elitism_factor=(0.12 0.25) # Taxa de preser
-mutation_rate=(0.1 0.25) 
+problems=("A/A-n32-k5.vrp" "A/A-n80-k10.vrp" "A/A-n46-k7.vrp" "B/B-n78-k10.vrp"  "B/B-n50-k8.vrp" )
+pop_size=(20 80 150) 
+tournament_size=(5 15) # Grau de competitividade para reprodução
+elitism_factor=(0.12 0.25 ) # Taxa de preservação da geração anterior
+mutation_rate=(0.1 0.25 0.4) 
 crossover_rate=(0.5 0.75) # Taxa de preservação da estrutura do parent1 no novo individuo
 crossover_type=("2x" "ux")
 
-for problem in "${problems[@]}"
-do for ps in "${pop_size[@]}"
-do for ts in "${tournament_size[@]}"
-do for ef in "${elitism_factor[@]}"
+for ts in "${tournament_size[@]}"
 do for mr in "${mutation_rate[@]}"
+do for problem in "${problems[@]}"
+do for ps in "${pop_size[@]}"
+do for ef in "${elitism_factor[@]}"
 do for ct in "${crossover_type[@]}" 
 do 
     if [ "$ct" = "ux" ]

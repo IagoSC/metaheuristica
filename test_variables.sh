3d92e31896e47801bcc6ff49fed51c7dd3c2d4ea
iagosc <iagosousavix@gmail.com>
2025-02-19 21:31:00 -0300

    Improve .sh

--- test_variables.sh
+++ test_variables.sh
@@ -3,11 +3,13 @@
 # Problem Set
 problems=("A/A-n32-k5.vrp" "A/A-n80-k10.vrp" "A/A-n46-k7.vrp" "B/B-n78-k10.vrp"  "B/B-n50-k8.vrp" )
 pop_size=(20 80 150) 
-tournament_size=(5 15) # Grau de competitividade para reprodução
-elitism_factor=(0.12 0.25 ) # Taxa de preservação da geração anterior
+tournament_size=(25) # Grau de competitividade para reprodução
+elitism_factor=(0.35) # Taxa de preservação da geração anterior
 mutation_rate=(0.1 0.25 0.4) 
 crossover_rate=(0.5 0.75) # Taxa de preservação da estrutura do parent1 no novo individuo
 crossover_type=("2x" "ux")
+
+table="new_table"
 
 for ts in "${tournament_size[@]}"
 do for mr in "${mutation_rate[@]}"
@@ -20,13 +22,13 @@ do
     then
         for cr in "${crossover_rate[@]}"
         do 
-            echo "python3 genetic_algorithm.py $problem $ps $ts $ef $mr $ct $cr"
+            cmd="python3 genetic_algorithm.py $table $problem $ps $ts $ef $mr $ct $cr"
 
-            python3 genetic_algorithm.py "$problem" "$ps" "$ts" "$ef" "$mr" "$ct" "$cr" >> output
+            sh -c "$cmd" >> output
         done
     else
-        echo "python3 genetic_algorithm.py $problem $ps $ts $ef $mr $ct $cr 0"
-        python3 genetic_algorithm.py "$problem" "$ps" "$ts" "$ef" "$mr" "$ct" 0 >> output
+        cmd="python3 genetic_algorithm.py $table $problem $ps $ts $ef $mr $ct $cr"
+        sh -c "$cmd" >> output
     fi
 done
 done
